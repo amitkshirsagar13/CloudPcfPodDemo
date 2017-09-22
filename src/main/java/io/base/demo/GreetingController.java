5c94affa724852e8c456a1dcfc65f6dbb4670278
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2017-09-22 14:54:56 +0530

    Working Demo with Kubernetes

---
+++ src/main/java/io/base/demo/GreetingController.java
@@ -0,0 +1,33 @@
+package io.base.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@Value("${server.address}")
+	private String serverName = null;
+
+	@Value("${server.port}")
+	private String serverPort = null;
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		StringBuilder msg = new StringBuilder();
+		msg.append(name);
+		msg.append(" from: ");
+		msg.append(serverName);
+		msg.append(":");
+		msg.append(serverPort);
+		msg.append("!!!");
+		return new Greeting(counter.incrementAndGet(), String.format(template, msg));
+	}
+}
